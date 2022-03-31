eafed7ca0694c0d244d66d406a878cb8eafc4868
jfernandesdev <jfernandes.dev@gmail.com>
2022-03-31 11:38:33 -0300

    home main banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import { Flex, Box, Text, Image } from '@chakra-ui/react';
+
 export default function Home() {
   return (
     <>
@@ -7,7 +9,34 @@ export default function Home() {
         <title>Home | World Trip</title>
       </Head>
 
-      <h1>I am Home!</h1>
+      <Flex
+        bgImg="/sky-background.svg"
+        w="100%"
+        justify="center"
+        h={325}
+        bgRepeat="no-repeat"
+        bgSize="cover"
+      >
+        <Flex maxWidth={1480} px="6" align="center" justify="space-between">
+          <Box maxWidth={450} mr="8">
+            <Text
+              as="h1"
+              fontSize={['2xl', '4xl']}
+              color="gray.50"
+              fontWeight={500}
+            >
+              5 Continentes, <br /> infinitas possibilidades.
+            </Text>
+            <Text fontSize="lg" color="gray.100" mt="4">
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.{' '}
+            </Text>
+          </Box>
+
+          <Image src="/airplane.svg" alt="Yellow Airplane" mt="17%" />
+        </Flex>
+      </Flex>
+
+      
     </>
   );
 }
